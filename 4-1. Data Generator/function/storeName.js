192553d38e236076c8d1a0ffb9281cac5cfdae29
iamjaeeuncho <redjoun@gmail.com>
2023-11-14 21:38:29 +0900

    node http

--- 4-1. Data Generator/function/storeName.js
+++ 4-1. Data Generator/function/storeName.js
@@ -1,6 +1,6 @@
 // 스토어 이름 생성
 export function storegen() {
-    let brands = ['스타벅스', '투썸플레이스', '커피빈', '이디야', '빽다방', "메가커피"];
+    let brands = ['스타벅스', '투썸플레이스', '커피빈', '이디야', '빽다방', '메가커피'];
     let branches = ['강남', '방배', '청담', '신림', '건대', '명동', '홍대', '신촌', '종로', '대학로', '노원', '동대문'];
   
     let brand = brands[Math.floor(Math.random() * brands.length)];

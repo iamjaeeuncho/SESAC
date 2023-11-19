0cf9e30d75d6388b1f3248407fbab7d9d8034cbb
iamjaeeuncho <redjoun@gmail.com>
2023-11-19 12:31:22 +0900

    nodejs express crud

--- 4-4. CRUD_Express/app.js
+++ 4-4. CRUD_Express/app.js
@@ -5,6 +5,8 @@ const path = require('path')
 const app = express();
 const port = 3000;
 
+const users = {};
+
 app.use(express.static('public'));
 
 // 1. 라우팅: 루트 경로 생성
@@ -23,20 +25,49 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/user', (req, res) => {
-    res.send(users)  // user 객체 나와야함
+    res.send(users)
 })
 
-
-app.post('/', (req, res) => {
-    res.send('Hello, World!\nPOST received')
+app.post('/user', (req, res) => {
+    let body = "";
+    // req에 data가 있을 때
+    req.on("data", (data) => { body += data });
+    // req data 처리가 끝날 때
+    req.on("end", () => {
+        // console.log("요청 온 내용은??", body);
+        const formData = JSON.parse(body);
+        // console.log("파싱한 후??", formData)
+        const currentDate = Date.now();
+        users[currentDate] = formData.name;
+    });
+    // 결과 response 주는 코드
+    res.send("등록 성공")
 })
 
-app.put('/', (req, res) => {
-    res.send('PUT received')
+app.put('/user/:id', (req, res) => {
+    const key = req.params.id;
+    
+    let body = "";
+    req.on("data", (data) => { body += data });
+    req.on("end", () => {
+        const formData = JSON.parse(body);
+        users[key] = formData.name;
+        console.log(users);
+    })
+
+    res.end("수정 성공")
 })
 
-app.delete('/', (req, res) => {
-    res.send('DELETE received')
+app.delete('/user/:id', (req, res) => {
+    const key = req.params.id;
+    delete users[key];
+    try {
+        res.send(JSON.stringify(users));
+    } catch (error) {
+        console.error("삭제 중 오류 발생", error);
+        res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
+        res.send("서버에서 알수없는 오류가 발생하여 삭제에 실패했습니다.");
+    }
 })
 
 // 특정 포트에서 서버 시작
